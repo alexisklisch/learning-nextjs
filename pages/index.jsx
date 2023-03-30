163978683f8028504e7e3314b6074f909ff5d601
alexisklisch <unbosquense@gmail.com>
2023-03-29 23:21:40 -0300

    Agrego página con palta específica

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { useState, useEffect } from "react"
 
 function Home() {
@@ -13,13 +14,13 @@ function Home() {
       <section style={{maxWidth: '512px', margin: '0 auto', display: 'grid', gap: '12px', gridTemplateColumns: '33.3% 33.4% 33.3%'}}>
         {avos.data?.map((avo) => {
           return (
-            <>
+            <Link href={`/product/${avo.id}`}>
               <article key={avo.id}>
                 <h3>{avo.name}</h3>
                 <span>{avo.sku}</span>
                 <h2>Precio: {avo.price}</h2>
               </article>
-            </>
+            </Link>
           )
         })}
       </section>
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,11 +1,21 @@
 import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
 
 function ProductItem() {
   const router = useRouter()
   const { id } = router.query
-  return (
-    <div>El id es {id}</div>
-  )
+  const [avo, setAvo] = useState([])
+
+  useEffect(() => {
+    fetch(`/api/avo/${id}`)
+      .then(res => res.json())
+      .then(data => setAvo(data))
+  },[])
+
+  return <>
+    <h1>{avo.data?.name}</h1>
+    <p>{avo.data?.attributes.description}</p>
+  </>
 }
 
 export default ProductItem
